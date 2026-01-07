7b13fd03165758e2d68173e4d8b2ae2dafbee8a2
hamza <hamzaprograme@gmail.com>
2026-01-07 10:33:36 +0000

    changing the dto to interfaces instead of classes

--- src/app/model/login-request.model.ts
+++ src/app/model/login-request.model.ts
@@ -1,9 +1,4 @@
-export class LoginRequest{
+export interface LoginRequest{
     email: string;
     password: string;
-
-    constructor(){
-        this.email = '';
-        this.password = '';
-    }
 }
